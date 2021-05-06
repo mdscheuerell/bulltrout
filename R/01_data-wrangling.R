2824874feb2847a2697546fc99dcae7ec02466c2
Mark Scheuerell <scheuerl@uw.edu>
2021-05-06 05:37:43 -0700

    creating ingest script

---
+++ R/01_data-wrangling.R
@@ -0,0 +1,14 @@
+## data munging for USFWS bull trout SSA
+
+## load libraries
+library(here)
+library(readr)
+library(dplyr)
+
+## set directories
+data_dir <- here("data")
+
+
+## get raw file names
+
+
